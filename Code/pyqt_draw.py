35680ff31bfb197cb8cbacb11f4b18956b35309a
Sma Cohen <cohen.samuel@wustl.edu>
2020-06-18 23:39:27 -0400

    added a pyqt window that you can draw on

--- Code/pyqt_draw.py
+++ Code/pyqt_draw.py
@@ -2,7 +2,7 @@
 import sys
 from PyQt5.QtWidgets import QMainWindow, QAction, QMenu, QApplication
 from PyQt5.QtGui import QIcon
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtGui, QtWidgets, QtCore
 
 # For Vertical Layouts
 from PyQt5.QtCore import pyqtSlot
@@ -18,32 +18,26 @@ from PyQt5.QtWidgets import QGroupBox    # Group Box
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure     # Figure
 
-class MainMenu(QMainWindow):
+class DrawingPad(QtWidgets.QLabel):
 
     def __init__(self):
         super().__init__()
 
-        self.Title = 'Draw your shape!'
         # self.left = 100
         # self.top = 100
         # self.width = 600
         # self.height = 300
-        self.label = QtWidgets.QLabel()
+        #self.label = QtWidgets.QLabel()
         canvas = QtGui.QPixmap(400, 300)
         canvas.fill()
-        self.label.setPixmap(canvas)
-        self.setCentralWidget(self.label)
+        #self.label.setPixmap(canvas)
+        self.setPixmap(canvas)
+        self.pen = QtGui.QPen()
+        self.pen_width = 5
         self.initial_point , self.next_point = None, None
-        self.initUI()
 
-    def initUI(self):
-        self.setWindowTitle(self.Title)
-        # self.setGeometry(self.left, self.top, self.width, self.height)
-        self.statusBar()
-        mainPage = self.menuBar()
-        fileMenu = mainPage.addMenu('File')
-
-        self.show()
+    def set_pen_width(self, w):
+        self.pen_width = w
 
     def mouseMoveEvent(self, a):
         if self.initial_point is None:
@@ -52,10 +46,9 @@ class MainMenu(QMainWindow):
             return
 
 
-        painter = QtGui.QPainter(self.label.pixmap())
-        pen = QtGui.QPen()
-        pen.setWidth(10)
-        painter.setPen(pen)
+        painter = QtGui.QPainter(self.pixmap())
+        self.pen.setWidth(self.pen_width)
+        painter.setPen(self.pen)
         painter.drawLine(self.initial_point, self.next_point, a.x(), a.y())
         painter.end()
         self.update()
@@ -67,11 +60,68 @@ class MainMenu(QMainWindow):
         self.initial_point= None
         self.initial_point = None
 
+class QLineWidthButton(QtWidgets.QPushButton):
+
+    def __init__(self, width):
+        super().__init__()
+        self.setFixedSize(QtCore.QSize(24, 24))
+        # self.width = width
+        # painter = QtGui.QPainter(self.pixmap())
+        # self.pen = QtGui.QPen()
+        # self.pen.setWidth(width)
+        # painter.setPen(self.pen)
+        # painter.drawLine(self.initial_point, self.next_point, a.x(), a.y())
+        # painter.end()
+
+
+        self.setStyleSheet("background-color: %s;" % '#000000')
+
+WIDTHS = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+]
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.Title = 'Draw your shape!'
+        self.drawing_pad = DrawingPad()
+
+        widg = QtWidgets.QWidget()
+        main = QtWidgets.QVBoxLayout()
+        widg.setLayout(main)
+        main.addWidget(self.drawing_pad)
+
+        width_box = QtWidgets.QHBoxLayout()
+        self.add_width(width_box)
+        main.addLayout(width_box)
+
+        self.setCentralWidget(widg)
+
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.Title)
+        # self.setGeometry(self.left, self.top, self.width, self.height)
+        self.statusBar()
+        mainPage = self.menuBar()
+        fileMenu = mainPage.addMenu('File')
+
+        self.show()
+
+    def add_width(self, layout):
+        for i in WIDTHS:
+            w = QLineWidthButton(i)
+            w.pressed.connect(lambda i=i: self.drawing_pad.set_pen_width(i))
+            layout.addWidget(w)
+
 
 def main():
     app = QApplication(sys.argv)
-    mn = MainMenu()
+    mn = DrawingPad()
+    window = MainWindow()
+    window.show()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
